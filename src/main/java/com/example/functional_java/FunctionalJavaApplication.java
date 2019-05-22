da62686f39b60ed4868f680c93b3454eea2109a5
정연빈 <yeonbn88@gmail.com>
2019-05-22 23:12:50 +0900

    second commit

---
+++ src/main/java/com/example/functional_java/FunctionalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.functional_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/functional_java/FunctionalJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.functional_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunctionalJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
